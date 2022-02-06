2b86fcb57b553dd8428d841c7bb14ef499f1ea8c
Roman Matsuiev <romanmatsuiev@gmail.com>
2022-02-06 13:39:22 +0200

    add put endpoint to WOrkerController

--- src/main/java/com/example/springbootproject/controller/WorkerController.java
+++ src/main/java/com/example/springbootproject/controller/WorkerController.java
@@ -2,20 +2,22 @@ package com.example.springbootproject.controller;
 
 import com.example.springbootproject.entity.Worker;
 import com.example.springbootproject.repository.WorkerRepository;
+import com.example.springbootproject.service.WorkerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Objects;
 
 @RestController
 public class WorkerController {
 
     @Autowired
     WorkerRepository repository;
+
+    @Autowired
+    WorkerService workerService;
 
     @PostMapping("/workers")
     public ResponseEntity<Worker> addWorker(@RequestBody Worker worker) {
@@ -25,5 +27,17 @@ public class WorkerController {
     @GetMapping("/workers")
     public List<Worker> getAllWorkers() {
         return repository.findAll();
+    }
+    
+    @PutMapping("/workers/{id}")
+    public ResponseEntity<Worker> updateWorker(@RequestBody Worker worker, @PathVariable int id) {
+        worker.setId(id);
+       Worker returnValue = workerService.updateDeparture(worker);
+
+        if (Objects.isNull(returnValue)) {
+            return ResponseEntity.notFound().build();
+        } else {
+            return ResponseEntity.ok(returnValue);
+        }
     }
 }
